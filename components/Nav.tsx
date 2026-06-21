627de69c442951571a1aaec5294315acde96ef49
Echo-007-sudo <bosspofii@gmail.com>
2026-06-21 19:42:14 +0100

    feat: changed login button to Admin login

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -43,7 +43,7 @@ export default function Nav() {
           href="/login"
           className="px-4 py-2 rounded-full bg-brand-purple text-white text-sm font-medium"
         >
-          Login
+          Admin
         </Link>
       </div>
     </nav>
