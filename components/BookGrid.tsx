452559848f231a79a0d54a1e5ff8127edd72c9ef
Echo-007-sudo <bosspofii@gmail.com>
2026-06-21 10:27:09 +0100

    center h2 in BookGrid

--- components/BookGrid.tsx
+++ components/BookGrid.tsx
@@ -12,7 +12,7 @@ export default function BookGrid({
 }) {
   return (
     <section className="container mx-auto px-8 py-16">
-      <h2 className="text-2xl font-semibold text-blue-900 mb-8">{title}</h2>
+      <h2 className="text-2xl font-semibold text-zinc-500 text-center mb-8">{title}</h2>
       <div className="grid grid-cols-2 sm:grid-cols-4 gap-6">
         {books.map((book) => (
           <Link
